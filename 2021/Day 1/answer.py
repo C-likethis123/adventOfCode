007a8da795bb3a15ade58bb4883d8be938ecee0a
Chow Jia Ying <chowjiaying211@gmail.com>
2021-12-01 20:13:10 +0800

    Answers for Day 1

--- 2021/Day 1/answer.py
+++ 2021/Day 1/answer.py
@@ -1,10 +1,21 @@
 with open('input') as f:
     items = [int(line.strip()) for line in f]
 
-increased = 0
-for i in range(1, len(items)):
-    if items[i - 1] < items[i]:
-        increased += 1
+def qn1():
+    increased = 0
+    for i in range(1, len(items)):
+        if items[i - 1] < items[i]:
+            increased += 1
+    return increased
 
-print(increased)
+
+def qn2():
+    increased_sums = 0
+    for i in range(3, len(items)):
+        if items[i] > items[i - 3]:
+            increased_sums += 1
+    return increased_sums
+    
+print(f"Part 1: {qn1()}")
+print(f"Part 2: {qn2()}")
 
